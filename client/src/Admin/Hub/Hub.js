b6f44d307f0591fb57b9046e13df64b4e7c6a1f1
Michael Sansone <only.sansone@gmail.com>
2023-04-21 16:26:30 -0400

    first pull to server

--- client/src/Admin/Hub/Hub.js
+++ client/src/Admin/Hub/Hub.js
@@ -45,9 +45,41 @@ class Hub extends React.Component {
     render() {
         const cards = this.state.cardList
             ? this.state.cardList.filter(this.isSearched(this.state.searchTerm)).map((card, i) => (
-                  <Link to={'/addCard/' + card._id} key={card._id} className={'hub ' + card.strain}>
-                      <img className="imgStyle" src={'/api/cards/image/' + card._id + '/company_image'} alt={'img' + i} />
-                      <div className="cardName">{card.name}</div>
+                  <Link to={'/addCard/' + card._id} key={card._id} className="hub">
+                      {/* <img className="imgStyle" src={'/api/cards/image/' + card._id + '/company_image'} alt={'img' + i} />
+                <div className="cardName">{card.name}</div> */}
+
+                      <div className="card card_hub" key={i}>
+                          <div className="front">
+                              <section>
+                                  <div className={'card__topContainer card__topContainer_hub ' + card.strain}>
+                                      <div className={'card__strain card__strain_hub ' + card.strain}>{card.strain}</div>
+                                      <div className={'card__strain-smtext card__strain-smtext_hub ' + card.strain}>{card.type}</div>
+                                      <div className={card.newCheckBtn ? 'card__newItem card__newItem_hub' : 'card__newItemHide'}>NEW</div>
+                                      <div
+                                          className={card.recommendCheckBtn ? 'card__reccoItem card__reccoItem_hub' : 'card__reccoItemHide'}
+                                      >
+                                          Manis Toke of choice
+                                      </div>
+                                  </div>
+                                  <div className="card__bottomContainer">
+                                      <div className="card__name card__name_hub">{card.name}</div>
+                                      {card.nameCross ? <div className="card__nameCross">{card.nameCross}</div> : null}
+                                      {card.company_image ? (
+                                          <div className="card__image card__image_hub">
+                                              <img src={'/api/cards/image/' + card._id + '/company_image'} alt="company_image" />
+                                          </div>
+                                      ) : (
+                                          ''
+                                      )}
+                                      <div className="card__price-container card__price-container_hub">
+                                          <div className="card__grams">{card.amount}</div>
+                                          <div className="card__price">${card.price}</div>
+                                      </div>
+                                  </div>
+                              </section>
+                          </div>
+                      </div>
                   </Link>
               ))
             : '';
