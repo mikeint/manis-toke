40cfa8f4c401f4c2d9f470442f8b46d9a7782c2d
Michael Sansone <only.sansone@gmail.com>
2023-06-15 22:56:58 -0400

    adad logs

--- server/server.js
+++ server/server.js
@@ -3,23 +3,23 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const users = require('./routes/api/users');
 const cards = require('./routes/api/cards');
-const passport = require('passport'); 
+const passport = require('passport');
 const cors = require('cors');
 const methodOverride = require('method-override');
 
 //const path = require('path');
 
-
- 
 const app = express();
 
 app.use(cors());
 
-app.use(bodyParser.urlencoded({
-    extended: false
-}))
-app.use(bodyParser.json())
-app.use(methodOverride('_method')); 
+app.use(
+    bodyParser.urlencoded({
+        extended: false,
+    }),
+);
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
 
 //FOR POINTING dependencies for index.html in build
 //app.use(express.static(path.join(__dirname, 'build')))
@@ -30,29 +30,29 @@ const db = require('./config/keys').mongoURI;
 mongoose
     .connect(db)
     .then(() => console.log(`connected to ${db}`))
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 
 // Passport middleware
 app.use(passport.initialize());
 // Passport Config
-require('./config/passport')(passport);//
-
+require('./config/passport')(passport); //
 
 //app.get('/', (req, res) => res.send("Hello World"));
 //app.get('/ping', (req, res) => res.json("pong"));
 
-
 // USE routes
 app.use('/api/users', users);
 app.use('/api/cards', cards);
-
 
 // FOR pointing node to the build folder index.html
 /* app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 }); */
 
-  
+app.use(function (req, res, next) {
+    console.log(req.method, req.url);
+    next();
+});
 
 const port = process.env.PORT || 5000;
 
